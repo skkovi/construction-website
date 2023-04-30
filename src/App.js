ad0cb6ae1bc4ef8bc83b97c473b543df8a2e19c7
kevinkoudelkaNagarro <kevin.koudelka01@nagarro.com>
2023-04-30 13:05:51 -0400

    more styling changes

--- src/App.js
+++ src/App.js
@@ -6,24 +6,35 @@ import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 
 function App() {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({delay: 4000})])
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 4000 })])
   return (
     <div className='app-container'>
       <div className='app-header-text'>
-          <h2>Your Home Improvement Specialist</h2>
-        </div>
+        <h2 className='homepageText'>Your Home <br/> Improvement Specialist</h2>
+      </div>
       <div className='app-centered-flex'>
         <div class="embla" ref={emblaRef}>
           <div class="embla__container">
             <div class="embla__slide">
               <img src={nyc} />
-              </div>
+            </div>
             <div class="embla__slide">
               <img src={LA} />
-              </div>
+            </div>
             <div class="embla__slide">
               <img src={seattle} />
-              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='app-welcome-box'>
+        <div className='app-welcome-textbox'>
+          <h4 className='welcome-text'>ABOUT</h4>
+          <h4 className='app-welcome-companyText'>Pat Moretti,<br/> inc.</h4>
+        </div>
+        <div className='app-welcome-descriptionbox'>
+          <div className='app-welcome-paragraphbox'>
+          <span className='welcome-description'>Pat Moretti, inc. Home Improvement Specialist, has been serving Westchester County, New York for over 25 years.</span>
           </div>
         </div>
       </div>
