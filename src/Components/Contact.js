7df41b1b6d58270eeeeff64b6b6864357b0c6c1f
kevinkoudelkaNagarro <kevin.koudelka01@nagarro.com>
2023-04-30 23:37:30 -0400

    added automated email service

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,41 @@
 import '../Css/Contact.css';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { send } from 'emailjs-com';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      name: e.target.elements.name.value,
+      email: e.target.elements.email.value,
+      message: e.target.elements.message.value,
+    });
+    if (!e.target.elements.name.value || !e.target.elements.email.value || !e.target.elements.message.value) {
+      alert('Please fill out all fields');
+      return;
+    }
+    send(
+      'service_jxo8fhd',
+      'template_vxfclr3',
+      formData,
+      'J8hf-A9o0J11Pul1G'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
   return (
     <div className='contact-flexbox-container'>
       <div className='contact-centered-flex'>
@@ -11,13 +45,13 @@ function Contact() {
           <span className='contactInfoGray'>Private tours of our 4,500 sq. ft. showroom are limited. We ask our guests to follow current health and safety protocols. We're looking forward to seeing you in person!</span>
         </div>
         <div className='bottomContainer'>
-          <div className="input-wrapper">
-            <input type="text" className='name-field' placeholder="Name" />
-            <input type="text" className='email-field' placeholder="Email" />
+          <form onSubmit={handleSubmit} className="input-wrapper">
+            <input type="text" name='name' className='name-field' placeholder="Name" />
+            <input type="text" name='email' pattern='[a-z0-9]+@[a-z]+\.[a-z]{2,3}' className='email-field' placeholder="Email" />
             <input type="text" className='subject-field' placeholder="Subject" />
-            <input type="text" className='message-field' placeholder="Message" />
-            <button className='sendEmailButton'>Send Email</button>
-          </div>
+            <input type="text" name='message' className='message-field' placeholder="Message" />
+            <button type='submit' className='sendEmailButton'>Send Email</button>
+          </form>
           <div className='contactInfo'>
             <h4 className='infoTextHeaders'>PHONE:<span className='contactInfoGrayLarge'> 1 (914) 738-8722</span></h4>
             <h4 className='infoTextHeaders'>EMAIL:<span className='contactInfoGrayLarge'> patmorettigc@gmail.com</span></h4>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,39 +5,40 @@ import phoneIcon from '../Images/phoneIcon.png'
 function Header() {
   return (
     <div className="Header">
-        <nav className="nav-section">
-        <Link to="/">
-          <img className='morettiLogo' src={moretti}/>
+      <div className='nav-section'>
+        <nav>
+          <Link to="/">
+            <img className='morettiLogo' src={moretti} />
           </Link>
-            <ul>
-                <li className ='ref-list'>
-                    <Link to="/company">
-                    <a className='nav-a'>Company</a>
-                    </Link>
-                    </li>
-                    <li className ='ref-list'>
-                    <Link to="/services">
-                    <a className='nav-a'>Services</a>
-                    </Link>
-                    </li>
-                    <li className ='ref-list'>
-                    <Link to="/about">
-                    <a className='nav-a'>Portfolio</a>
-                    </Link>
-                    </li>
-                    <li className ='ref-list'>
-                    <Link to="/contact">
-                    <a className='nav-a'>Contact</a>
-                    </Link>
-                    </li>
-                    <div className='callNow'>
-                      <img className='phoneIcon' src={phoneIcon}/>
-                      <span>1 (914) 738-8722</span>
-                    </div>
-            </ul>
+          <ul className='navButton'>
+            <li className='ref-list'>
+              <Link to="/">
+                <a className='nav-a'>Home</a>
+              </Link>
+            </li>
+            <li className='ref-list'>
+              <Link to="/services">
+                <a className='nav-a'>Services</a>
+              </Link>
+            </li>
+            <li className='ref-list'>
+              <Link to="/about">
+                <a className='nav-a'>Portfolio</a>
+              </Link>
+            </li>
+            <li className='ref-list'>
+              <Link to="/contact">
+                <a className='nav-a'>Contact</a>
+              </Link>
+            </li>
+            <div className='callNow'>
+              <img className='phoneIcon' src={phoneIcon} />
+              <span>1 (914) 738-8722</span>
+            </div>
+          </ul>
         </nav>
+      </div>
     </div>
   );
 }
-
 export default Header;
