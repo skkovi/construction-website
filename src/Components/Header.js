2b56a55e97aede594721a576d564ee0e68f43e62
kevinkoudelkaNagarro <kevin.koudelka01@nagarro.com>
2023-05-02 19:28:57 -0400

    added services page and played with header css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,37 +6,35 @@ function Header() {
   return (
     <div className="Header">
       <div className='nav-section'>
-        <nav>
-          <Link to="/">
-            <img className='morettiLogo' src={moretti} />
-          </Link>
-          <ul className='navButton'>
-            <li className='ref-list'>
-              <Link to="/">
-                <a className='nav-a'>Home</a>
-              </Link>
-            </li>
-            <li className='ref-list'>
-              <Link to="/services">
-                <a className='nav-a'>Services</a>
-              </Link>
-            </li>
-            <li className='ref-list'>
-              <Link to="/about">
-                <a className='nav-a'>Portfolio</a>
-              </Link>
-            </li>
-            <li className='ref-list'>
-              <Link to="/contact">
-                <a className='nav-a'>Contact</a>
-              </Link>
-            </li>
-            <div className='callNow'>
-              <img className='phoneIcon' src={phoneIcon} />
-              <span>1 (914) 738-8722</span>
-            </div>
-          </ul>
-        </nav>
+        <Link to="/">
+          <img className='morettiLogo' src={moretti} />
+        </Link>
+        <ul className='navButton'>
+          <li className='ref-list'>
+            <Link to="/">
+              <a className='nav-a'>Home</a>
+            </Link>
+          </li>
+          <li className='ref-list'>
+            <Link to="/services">
+              <a className='nav-a'>Services</a>
+            </Link>
+          </li>
+          <li className='ref-list'>
+            <Link to="/about">
+              <a className='nav-a'>Portfolio</a>
+            </Link>
+          </li>
+          <li className='ref-list'>
+            <Link to="/contact">
+              <a className='nav-a'>Contact</a>
+            </Link>
+          </li>
+          <div className='callNow'>
+            <img className='phoneIcon' src={phoneIcon} />
+            <span>1 (914) 738-8722</span>
+          </div>
+        </ul>
       </div>
     </div>
   );
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,10 +1,64 @@
-import '../Css/Header.css';
+import '../Css/Services.css';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
 
 function Services() {
   return (
-    <div>
+    <div className='services-container'>
+      <div className='services-header'>
+        <div className='services-descriptionContainer'>
+          <div className='services-titleContainer'>
+            <h4 className='services-title2'>OUR</h4>
+            <h2 className='services-title'>SERVICES</h2>
+          </div>
+          <div className='services-description'>
+            <span className='services-descriptionText'>Our unified team of architects, designers, and construction professionals brings a thoughtful approach to your project as a whole, and to all the details that make your home feel uniquely yours. Each project we undertake starts with you – what are your needs, desires, and dreams for your home? Let’s explore how to make your home more livable and beautiful today.</span>
+          </div>
+        </div>
+        <div className='services-cardContainerLarge'>
+          <div className='services-cards'>
+            <div className='services-cardsTextContainer'>
+              HELLO
+            </div>
+          </div>
+          <div className='services-cards'>
+            <div className='services-cardsTextContainer'>
+              HELLO
+            </div>
+          </div>
+          <div className='services-cards'>
+            <div className='services-cardsTextContainer'>
+              HELLO
+            </div>
+          </div>
+          <div className='services-specific'>
+            <div className='services-textCards'>
+              Test
+            </div>
+            <div className='services-textCards'>
+              Test
+            </div>
+            <div className='services-textCards'>
+              Test
+            </div>
+          </div>
+          <div className='services-cards'>
+            <div className='services-cardsTextContainer'>
+              HELLO
+            </div>
+          </div>
+          <div className='services-cards'>
+            <div className='services-cardsTextContainer'>
+              HELLO
+            </div>
+          </div>
+          <div className='services-cards'>
+            <div className='services-cardsTextContainer'>
+              HELLO
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
