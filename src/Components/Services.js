599e7ac2996acf84813dd7816a41f19bf39c7c43
kevinkoudelkaNagarro <kevin.koudelka01@nagarro.com>
2023-05-19 13:59:14 -0400

    fixed services grid and added mobile support

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -20,66 +20,75 @@ function Services() {
             <span className='services-descriptionText'>Our unified team of architects, designers, and construction professionals brings a thoughtful approach to your project as a whole, and to all the details that make your home feel uniquely yours. Each project we undertake starts with you – what are your needs, desires, and dreams for your home? Let’s explore how to make your home more livable and beautiful today.</span>
           </div>
         </div>
-        <div className='services-cardContainerLarge'>
-          <div className='services-cards'>
+      </div>
+      <div className='services-gridContainer'>
+        <div className='services-cardItems'>
+          <div className='services-fullCard'>
             <img className='services-images' src={kitchen2} alt='' />
             <div className='services-cardsTextContainer'>
               KITCHEN
-            </div>
-          </div>
-          <div className='services-cards'>
-            <img className='services-images' src={paint} alt='' />
-            <div className='services-cardsTextContainer'>
-              PAINTING
-            </div>
-          </div>
-          <div className='services-cards'>
-            <img className='services-images' src={plumbing} alt='' />
-            <div className='services-cardsTextContainer'>
-              PLUMBING
-            </div>
-          </div>
-          <div className='services-specific'>
-            <div className='services-textCards'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-            <div className='services-textCards'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-            <div className='services-textCards'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-          </div>
-          <div className='services-cards'>
-            <img className='services-images' src={kitchen2} alt='' />
-            <div className='services-cardsTextContainer'>
-              HELLO
-            </div>
-          </div>
-          <div className='services-cards'>
-            <img className='services-images' src={kitchen2} alt='' />
-            <div className='services-cardsTextContainer'>
-              HELLO
-            </div>
-          </div>
-          <div className='services-cards'>
-            <img className='services-images' src={kitchen2} alt='' />
-            <div className='services-cardsTextContainer'>
-              HELLO
-            </div>
-          </div>
-          <div className='services-specific'>
-            <div className='services-textCards'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-            <div className='services-textCards'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
             </div>
             <div className='services-textCards'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
             </div>
           </div>
         </div>
+        <div className='services-cardItems'>
+          <div className='services-fullCard'>
+            <img className='services-images' src={paint} alt='' />
+            <div className='services-cardsTextContainer'>
+              PAINTING
+            </div>
+            <div className='services-textCards'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </div>
+          </div>
+        </div>
+        <div className='services-cardItems'>
+          <div className='services-fullCard'>
+            <img className='services-images' src={plumbing} alt='' />
+            <div className='services-cardsTextContainer'>
+              PLUMBING
+            </div>
+            <div className='services-textCards'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </div>
+          </div>
+        </div>
+        <div className='services-cardItems'>
+          <div className='services-fullCard'>
+            <img className='services-images' src={kitchen2} alt='' />
+            <div className='services-cardsTextContainer'>
+              KITCHEN
+            </div>
+            <div className='services-textCards'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </div>
+          </div>
+        </div>
+        <div className='services-cardItems'>
+          <div className='services-fullCard'>
+            <img className='services-images' src={kitchen2} alt='' />
+            <div className='services-cardsTextContainer'>
+              KITCHEN
+            </div>
+            <div className='services-textCards'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </div>
+          </div>
+        </div>
+        <div className='services-cardItems'>
+          <div className='services-fullCard'>
+            <img className='services-images' src={kitchen2} alt='' />
+            <div className='services-cardsTextContainer'>
+              KITCHEN
+            </div>
+            <div className='services-textCards'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </div>
+          </div>
+        </div>
+
       </div>
     </motion.div>
   );
